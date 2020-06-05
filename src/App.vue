6a74d0d93a306c6ec845bde413de6c3c5c86a75d
Sergio Azócar <sergioazoc@gmail.com>
2020-06-05 02:17:42 -0400

    add loading

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,64 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.type{
+  margin: 0 5px;
+  padding: 6px 10px;
+  color: #ffffff;
+  border-radius: 10px;
+}
+.normal{
+  background-color: #a4acaf;
+}
+.fighting{
+  background-color: #d56723;
+}
+.flying{
+  background: linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%);
+}
+.poison{
+  background-color: #b97fc9;
+}
+.ground{
+  background: linear-gradient(180deg, #f7de3f 50%, #ab9842 50%);
+}
+.rock{
+  background-color: #a38c21;
+}
+.bug{
+  background-color: #729f3f;
+}
+.ghost{
+  background-color: #7b62a3;
+}
+.steel{
+  background: linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%);
+}
+.fire{
+  background-color: #fd7d24;
+}
+.water{
+  background-color: #4592c4;
+}
+.grass{
+  background-color: #9bcc50;
+}
+.electric{
+  background-color: #eed535;
+}
+.psychic{
+  background-color: #f366b9;
+}
+.ice{
+  background-color: #51c4e7;
+}
+.dragon{
+  background: linear-gradient(180deg, #53a4cf 50%, #f16e57 50%);
+}
+.dark{
+  background-color: #707070;
+}
+.fairy{
+  background-color: #fdb9e9;
+}
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,11 +3,13 @@
     <h1>Busca un Pokémon (nombre o número):</h1>
     <input v-model="pokemonToFind" type="text">
     <button @click="findPokemon(pokemonToFind)">Buscar</button>
-    <div v-if="pokemon">
+    <div v-if="pokemon && !error">
       <h2>N°{{pokemon.id}} {{pokemon.species.name.toUpperCase()}}</h2>
       <img :src="pokemon.sprites.front_default"/>
-      <p>Tipo <span class="type" :class="type.type.name" v-for="(type, index) of pokemon.types" :key="index">{{type.type.name}}</span></p>
+      <p>Tipo: <span class="type" :class="type.type.name" v-for="(type, index) of pokemon.types" :key="index">{{type.type.name}}</span></p>
     </div>
+    <div class="error" v-else-if="error">{{error}}</div>
+    <img v-if="loading" class="pokeball" src="/pokeball.svg" alt="">
   </div>
 </template>
 
@@ -19,77 +21,50 @@ export default {
     return {
       url: "https://pokeapi.co/api/v2/pokemon/",
       pokemon: null,
-      pokemonToFind: ""
+      pokemonToFind: "",
+      error: "",
+      loading: false
     }
   },
   methods: {
     findPokemon: function(q){
-      axios.get("https://pokeapi.co/api/v2/pokemon/"+q)
-        .then(response => (this.pokemon = response.data))
+      this.error = ""
+      this.pokemon = null
+      this.loading = true
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/"+q)
+        .then(response => (this.pokemon = response.data, this.loading = false))
+        .catch( () => (this.error = "Pokémon no encontrado 404", this.loading = false))
     }
   }
 };
 </script>
 
 <style scoped>
-.type{
-  margin: 0 5px;
-  padding: 6px 10px;
-  color: #ffffff;
-  border-radius: 10px;
+.error{
+  margin-top: 30px;
 }
-.normal{
-  background-color: #a4acaf;
+
+.pokeball{
+  display: block;
+  width: 60px;
+  height: 60px;
+  margin: 50px auto 0 auto;
+  animation:movePokeball 1s linear infinite both;
 }
-.fighting{
-  background-color: #d56723;
-}
-.flying{
-  background: linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%);
-}
-.poison{
-  background-color: #b97fc9;
-}
-.ground{
-  background: linear-gradient(180deg, #f7de3f 50%, #ab9842 50%);
-}
-.rock{
-  background-color: #a38c21;
-}
-.bug{
-  background-color: #729f3f;
-}
-.ghost{
-  background-color: #7b62a3;
-}
-.steel{
-  background: linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%);
-}
-.fire{
-  background-color: #fd7d24;
-}
-.water{
-  background-color: #4592c4;
-}
-.grass{
-  background-color: #9bcc50;
-}
-.electric{
-  background-color: #eed535;
-}
-.psychic{
-  background-color: #f366b9;
-}
-.ice{
-  background-color: #51c4e7;
-}
-.dragon{
-  background: linear-gradient(180deg, #53a4cf 50%, #f16e57 50%);
-}
-.dark{
-  background-color: #707070;
-}
-.fairy{
-  background-color: #fdb9e9;
+
+@keyframes movePokeball{
+  0%{
+    transform:translateX(0) rotate(0)
+  }
+  15%{
+    transform:translatex(-10px) rotate(-5deg)
+  }
+  30%{
+    transform:translateX(10px) rotate(5deg)
+  }
+  45%{
+    transform:translatex(0) rotate(0)
+  }
 }
 </style>
